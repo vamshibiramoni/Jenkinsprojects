24f443fd36b214fde65c889dbfefbf46a48108e7
Daily <Daily@10.0.0.16>
2018-04-19 22:56:09 -0400

    this is the jenkins file projects

---
+++ src/Retangulator.java
@@ -0,0 +1,17 @@
+
+public class Retangulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int length = Integer.parseInt(args[0]);
+    int width = Integer.parseInt(args[1]);
+    
+    Rectangle myRectangle = new Rectangle (length , width );
+    
+    String output= String.format(" *** your rectangle ***\n\nLength :%d\nWidth:%d\n nArea: %d\n Perimeter :%d\n\n", myRectangle.length, myRectangle.width,myRectangle.getArea(),myRectangle.getPerimeter());
+    
+    System.out.println(output);
+    
+	}
+
+}
